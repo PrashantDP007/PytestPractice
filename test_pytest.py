2e9f99f8d8dcd3fb43e3f44469352c1055eddfc0
PrashantDP007 <159519077+PrashantDP007@users.noreply.github.com>
2026-04-18 21:32:23 +0530

    Fix print statement syntax in test_pytest.py

--- test_pytest.py
+++ test_pytest.py
@@ -55,7 +55,7 @@ def test_google_search():
     driver.find_element(By.XPATH, "//textarea[contains(@jsname,'yZiJ')]").send_keys("Prashant Pardeshi")
 
     search_locator = (By.XPATH, "//div[@jsname='VlcLAe']//input[@value='Google Search']")
-    print("Test Jenkins Trigger)
+    print("Test Jenkins Trigger")
     search = wait.until(EC.visibility_of_element_located(search_locator)) # after explicit wait
     search.click()
 
