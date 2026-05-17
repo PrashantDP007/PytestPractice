3e39ccb6131b1f2db2be48b279a62811ba46229d
PrashantDP007 <prashantdpardeshi04@gmail.com>
2026-05-17 22:19:26 +0530

    Commit to test  trigger

--- test_pytest.py
+++ test_pytest.py
@@ -56,6 +56,7 @@ def test_google_search():
 
     search_locator = (By.XPATH, "//div[@jsname='VlcLAe']//input[@value='Google Search']")
     print("Test Jenkins Trigger")
+    print("Adding this line to check the git commit jenkins trigger")
     search = wait.until(EC.visibility_of_element_located(search_locator)) # after explicit wait
     search.click()
 
